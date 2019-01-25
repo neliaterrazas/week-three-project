b1514c62356081dbd4e0f8673eadc51f16f9f637
Nelia Terrazas <terrazasnelia@gmail.com>
2019-01-25 15:18:24 -0800

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ var rules = function(num) {
   var answer = [ ]; //empty array
 
   for (i=0; i<=num; i++) {
+    if(i.toString().includes("3")) {
+      answer.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    }else if(i.toString().includes("2")) {
+      answer.push("Boop");
+    }else if(i.toString().includes("1")) {
+      answer.push("Beep");
+    }else
+      answer.push(" "+ i);
+    }
+    return answer;
+});
